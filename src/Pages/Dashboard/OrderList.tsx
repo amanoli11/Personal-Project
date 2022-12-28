ada8426e2a48200c9ab6b1a611bb348e2452539c
Shakti Dewan <shaktiyakhha@gmail.com>
2022-12-28 23:52:57 +0545

    commit

--- src/Pages/Dashboard/OrderList.tsx
+++ src/Pages/Dashboard/OrderList.tsx
@@ -1,13 +1,64 @@
+import { DeleteOutlined } from "@ant-design/icons";
+import { Button, Col, Row,Space } from "antd";
 import React from "react";
-import '../../styles/orderList.css'
+import "../../styles/orderList.css";
 
 export default function OrderList() {
   return (
     <div>
-      <div className="titleSection">
-        <div>
-          <h1>OrderList</h1>
-        </div>
+      <div className="orderHeader">
+        <Row>
+          <Col className="gutter-row" span={14}>
+            <div className="title">
+              <h1>OrderList</h1>
+              <p>Transaction #00391</p>
+            </div>
+          </Col>
+          <Col className="gutter-row" span={10}>
+            <div className="icons">
+              <div className="iconBox">
+                <Button block>
+                  <DeleteOutlined style={{ fontSize: "20px", color: "#08c" }} />
+                </Button>
+              </div>
+              <div className="iconBox">
+                <Button block>
+                  <DeleteOutlined style={{ fontSize: "20px", color: "#08c" }} />
+                </Button>
+              </div>
+              <div className="iconBox">
+                <Button block>
+                  <DeleteOutlined style={{ fontSize: "20px", color: "#08c" }} />
+                </Button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </div>
+      <div className="optionsSection">
+        <Row>
+          <Col className="gutter-row" span={8}>
+          <Space direction="vertical" style={{ width: '60%' }}>
+            <Button type="primary" block size="middle" className="OptionButtonStyle">
+              Primary
+            </Button>
+            </Space>
+          </Col>
+          <Col className="gutter-row" span={8}>
+          <Space direction="vertical" style={{ width: '60%' }}>
+            <Button type="primary" block size="middle" className="OptionButtonStyle">
+              Primary
+            </Button>
+            </Space>
+          </Col>
+          <Col className="gutter-row" span={8}>
+          <Space direction="vertical" style={{ width: '60%' }}>
+            <Button type="primary" block size="middle" className="OptionButtonStyle">
+              Primary
+            </Button>
+            </Space>
+          </Col>
+        </Row>
       </div>
     </div>
   );
