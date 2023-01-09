f46abb97711dad8c30b89ee1d976349115188230
Shakti Dewan <shaktiyakhha@gmail.com>
2023-01-09 22:39:09 +0545

    Scroll a certain component only

--- src/Pages/FoodItems.tsx
+++ src/Pages/FoodItems.tsx
@@ -5,11 +5,74 @@ import CardComponent from "../components/CardComponent";
 const FoodItems = () => {
   return (
     <>
-      <div>
+      <div className="foodSection">
         <h1>Food (44) items</h1>
         <Row>
           <div className="cardSection">
             <CardComponent 
+              url="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              name="Mighty Zinger"
+              price={25}
+            />
+          </div>
+          <div className="cardSection">
+            <CardComponent
+              url="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              name="Mighty Zinger"
+              price={25}
+            />
+          </div>
+          <div className="cardSection">
+            <CardComponent
+              url="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              name="Mighty Zinger"
+              price={25}
+            />
+          </div>
+          <div className="cardSection">
+            <CardComponent
+              url="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              name="Mighty Zinger"
+              price={25}
+            />
+          </div>
+          <div className="cardSection">
+            <CardComponent
+              url="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              name="Mighty Zinger"
+              price={25}
+            />
+          </div>
+          <div className="cardSection">
+            <CardComponent
+              url="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              name="Mighty Zinger"
+              price={25}
+            />
+          </div>
+          <div className="cardSection">
+            <CardComponent
+              url="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              name="Mighty Zinger"
+              price={25}
+            />
+          </div>
+          <div className="cardSection">
+            <CardComponent
+              url="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              name="Mighty Zinger"
+              price={25}
+            />
+          </div>
+          <div className="cardSection">
+            <CardComponent
+              url="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              name="Mighty Zinger"
+              price={25}
+            />
+          </div>
+          <div className="cardSection">
+            <CardComponent
               url="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
               name="Mighty Zinger"
               price={25}
